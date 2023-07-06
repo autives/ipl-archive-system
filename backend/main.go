f12ce4a446a8fc6a7b954d9ec12db3dde5c2783f
autives <ashubohara888@gmail.com>
2023-07-06 23:29:37 +0545

    player stats

--- backend/main.go
+++ backend/main.go
@@ -31,6 +31,30 @@ type Player struct {
 	BowlingAffinity string `json:"bowlingAffinity"`
 }
 
+type BattingStats struct {
+	Runs       int     `json:"runs"`
+	StrikeRate float32 `json:"strikeRate"`
+	Average    float32 `json:"average"`
+	Balls      int     `json:"balls"`
+	Sixes      int     `json:"sixes"`
+	Fours      int     `json:"fours"`
+	Innings    int     `json:"innings"`
+	Fifties    int     `json:"fifties"`
+	Centuries  int     `json:"centuries"`
+	NotOut     int     `json:"notOut"`
+}
+
+type BowlingStats struct {
+	Innings     int `json:"innings"`
+	Overs       int `json:"overs"`
+	Runs        int `json:"runs"`
+	MaidenOvers int `json:"maidenOvers"`
+	Wickets     int `json:"wickets"`
+	Extras      int `json:"extras"`
+	Average     int `json:"average"`
+	Economy     int `json:"economy"`
+}
+
 func readPlayerData(db *sql.DB, id int) Player {
 	var res Player
 	var dob time.Time
@@ -41,6 +65,54 @@ func readPlayerData(db *sql.DB, id int) Player {
 
 	res.Age = int((time.Since(dob).Hours() / (24 * 365.0)))
 	return res
+}
+
+func readBattingStats(db *sql.DB, id int) BattingStats {
+	var stats BattingStats
+
+	query := fmt.Sprintf("select sum(runs) as runs, sum(fours) as fours, sum(sixes) as sixes, sum(ballPlayed) as balls from BattingInnings where playerId = %d", id)
+	queryRowRes := db.QueryRow(query)
+	queryRowRes.Scan(&stats.Runs, &stats.Sixes, &stats.Fours, &stats.Balls)
+
+	query = fmt.Sprintf("select count(inningsId) from BattingInnings where playerId = %d", id)
+	queryRowRes = db.QueryRow(query)
+	queryRowRes.Scan(&stats.Innings)
+
+	query = fmt.Sprintf("select count(runs) from BattingInnings where playerId = %d and runs > 50", id)
+	queryRowRes = db.QueryRow(query)
+	queryRowRes.Scan(&stats.Fifties)
+
+	query = fmt.Sprintf("select count(runs) from BattingInnings where playerId = %d and runs > 100", id)
+	queryRowRes = db.QueryRow(query)
+	queryRowRes.Scan(&stats.Centuries)
+
+	query = fmt.Sprintf("select count(runs) from BattingInnings where playerId = %d and \"out\" is null", id)
+	queryRowRes = db.QueryRow(query)
+	queryRowRes.Scan(&stats.NotOut)
+
+	stats.StrikeRate = float32(stats.Runs * 100.0 / stats.Balls)
+	stats.Average = float32(float32(stats.Runs) / float32(stats.Innings))
+
+	return stats
+}
+
+func readBowlingStats(db *sql.DB, id int) BowlingStats {
+	var stats BowlingStats
+
+	query := fmt.Sprintf("select count(inningsId) as innings, sum(runs) as runs, (sum(bowlsDelivered)/6) as overs, sum(wicketsTaken) as wickets, (sum(wides) + sum(noBalls) + sum(legBy) + sum(\"by\")) as extras, sum(maidenOvers) as maidenOvers from BowlingInnings where playerId = %d", id)
+	queryRowRes := db.QueryRow(query)
+	queryRowRes.Scan(&stats.Innings, &stats.Runs, &stats.Overs, &stats.Wickets, &stats.Extras, &stats.MaidenOvers)
+
+	query = fmt.Sprintf("select count(inningsId) from BowlingInnings where playerId = %d", id)
+	queryRowRes = db.QueryRow(query)
+	queryRowRes.Scan(&stats.Innings)
+
+	fmt.Println(stats)
+
+	stats.Average = stats.Runs / stats.Wickets
+	stats.Economy = stats.Runs / stats.Overs
+
+	return stats
 }
 
 func player(db *sql.DB) gin.HandlerFunc {
@@ -58,6 +130,13 @@ func player(db *sql.DB) gin.HandlerFunc {
 
 		res := make(map[string]any)
 		res["data"] = data
+
+		if data.Affinity == "BATSMAN" || data.Affinity == "ALL_ROUNDER" || data.Affinity == "WICKET_KEEPER_BATSMAN" {
+			res["battingStats"] = readBattingStats(db, id)
+		}
+		if data.Affinity == "ALL_ROUNDER" || data.Affinity == "BOWLER" {
+			res["bowlingStats"] = readBowlingStats(db, id)
+		}
 
 		c.JSON(http.StatusOK, res)
 	}
